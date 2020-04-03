816228da9b68e2af1022ad2fa3b58e56ff871fc7
Anthony NGUYEN <anthony.ng@hotmail.fr>
2020-04-03 18:19:25 +0200

    Add controller and view. Closes #3

---
+++ src/main/java/guru/springframework/sfgjokes/controllers/JokeController.java
@@ -0,0 +1,22 @@
+package guru.springframework.sfgjokes.controllers;
+
+import guru.springframework.sfgjokes.services.FetchJokeImpl;
+import guru.springframework.sfgjokes.services.FetchJokeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokeController {
+    private final FetchJokeImpl fetchJokeImpl;
+
+    public JokeController(FetchJokeImpl fetchJokeImpl) {
+        this.fetchJokeImpl = fetchJokeImpl;
+    }
+
+    @RequestMapping("")
+    public String getJokes(Model model) {
+        model.addAttribute("joke", fetchJokeImpl.getJoke());
+        return "jokes";
+    }
+}
