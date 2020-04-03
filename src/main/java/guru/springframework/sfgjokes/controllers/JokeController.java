a328803733fae77dff743962601985dbae5f4b9a
Anthony NGUYEN <anthony.ng@hotmail.fr>
2020-04-03 19:19:27 +0200

    Add optional autowired and multiple path in mapping annotation.

--- src/main/java/guru/springframework/sfgjokes/controllers/JokeController.java
+++ src/main/java/guru/springframework/sfgjokes/controllers/JokeController.java
@@ -2,6 +2,7 @@ package guru.springframework.sfgjokes.controllers;
 
 import guru.springframework.sfgjokes.services.FetchJokeImpl;
 import guru.springframework.sfgjokes.services.FetchJokeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class JokeController {
     private final FetchJokeImpl fetchJokeImpl;
 
+    @Autowired // Optional
     public JokeController(FetchJokeImpl fetchJokeImpl) {
         this.fetchJokeImpl = fetchJokeImpl;
     }
 
-    @RequestMapping("")
+    @RequestMapping({"", "/"})
     public String getJokes(Model model) {
         model.addAttribute("joke", fetchJokeImpl.getJoke());
         return "jokes";
