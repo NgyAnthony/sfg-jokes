d0dd33561ef4591b0b8c8b3d314453414235cf37
Anthony NGUYEN <anthony.ng@hotmail.fr>
2020-04-03 16:57:39 +0200

    Initialize Spring Boot. Closes #1

---
+++ src/test/java/guru/springframework/sfgjokes/SfgJokesApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgjokes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgJokesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
